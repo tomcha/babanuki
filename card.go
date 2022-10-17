93ea2429b2ecdf8333bee831c75b08a459d55c15
tomcha <tomcha@tomcha.net>
2022-10-18 08:28:27 +0900

    Card ストラクトとテスト実装

---
+++ card.go
@@ -0,0 +1,52 @@
+package main
+
+import "strconv"
+
+const (
+	JOKER = iota
+	SUIT_SPADE
+	SUIT_DIAMOND
+	SUIT_CLUB
+	SUIT_HEART
+)
+
+type Card struct {
+	suit_   int
+	number_ int
+}
+
+func (c Card) getNumber() (int, error) {
+	return c.number_, nil
+}
+
+func (c Card) getSuit() (int, error) {
+	return c.suit_, nil
+}
+
+func (c Card) String() string {
+	var s string
+	switch c.suit_ {
+	case SUIT_SPADE:
+		s = "S"
+	case SUIT_DIAMOND:
+		s = "D"
+	case SUIT_CLUB:
+		s = "C"
+	case SUIT_HEART:
+		s = "H"
+	}
+
+	switch c.number_ {
+	case 1:
+		s += "A"
+	case 11:
+		s += "J"
+	case 12:
+		s += "Q"
+	case 13:
+		s += "K"
+	default:
+		s += strconv.Itoa(c.number_)
+	}
+	return s
+}
--- card_test.go
+++ card_test.go
@@ -4,10 +4,9 @@ import (
 	"testing"
 )
 
-func TestCard(t *testing.T) {
+func TestGetNumber(t *testing.T) {
 	var s int = SUIT_DIAMOND
 	var n int = 3
-
 	c := Card{s, n}
 
 	result, err := c.getNumber()
@@ -15,6 +14,70 @@ func TestCard(t *testing.T) {
 		t.Fatalf("failed test %#v", err)
 	}
 	if result != 3 {
-		t.Fatal("faild test")
+		t.Fatal("failed test")
+	}
+
+	s = SUIT_DIAMOND
+	n = 10
+	c = Card{s, n}
+
+	result, err = c.getNumber()
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != 10 {
+		t.Fatal("failed test")
+	}
+}
+
+func TestGetSuit(t *testing.T) {
+	var s int = SUIT_HEART
+	var n int = 10
+	c := Card{s, n}
+
+	result, err := c.getSuit()
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != SUIT_HEART {
+		t.Fatal("failed test")
+	}
+
+	s = SUIT_CLUB
+	n = 5
+	c = Card{s, n}
+	result, err = c.getSuit()
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != SUIT_CLUB {
+		t.Fatal("failed test")
+	}
+}
+
+func TestString(t *testing.T) {
+	var s int = SUIT_SPADE
+	var n int = 1
+	var r string
+	c := Card{s, n}
+	r = c.String()
+	if r != "SA" {
+		t.Fatal("failed test")
+	}
+
+	s = SUIT_HEART
+	n = 11
+	c = Card{s, n}
+	r = c.String()
+	if r != "HJ" {
+		t.Fatal("failed test")
+	}
+
+	s = SUIT_DIAMOND
+	n = 10
+	c = Card{s, n}
+	r = c.String()
+	if r != "D10" {
+		t.Fatal("failed test")
 	}
 }
