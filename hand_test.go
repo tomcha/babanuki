ffbbc828963cae626cd6279302a539068364c6f8
tomcha <tomcha@tomcha.net>
2022-10-21 08:25:05 +0900

    pickCard テスト実装

--- hand_test.go
+++ hand_test.go
@@ -13,6 +13,19 @@ func TestAddCard(t *testing.T) {
 	h.addCard(c2)
 
 	if h.hand_[1] != c2 {
-		t.Fatal("test false")
+		t.Fatal("test fale")
+	}
+}
+
+func TestPickCard(t *testing.T) {
+	c1 := Card{SUIT_DIAMOND, 10}
+	c2 := Card{SUIT_CLUB, 3}
+
+	h := Hand{}
+	h.addCard(c1)
+	h.addCard(c2)
+
+	if h.pickCard() != c1 {
+		t.Fatal("test fale")
 	}
 }
