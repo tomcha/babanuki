f538e0f676a53bfb6c65ba34cf8ec3772a8addd3
tomcha <tomcha@tomcha.net>
2022-10-21 15:18:31 +0900

    add pickCardメソッドの Test

--- hand_test.go
+++ hand_test.go
@@ -20,16 +20,18 @@ func TestAddCard(t *testing.T) {
 func TestPickCard(t *testing.T) {
 	c1 := Card{SUIT_DIAMOND, 10}
 	c2 := Card{SUIT_CLUB, 3}
+	c3 := Card{SUIT_HEART, 12}
 
 	h := Hand{}
 	h.addCard(c1)
 	h.addCard(c2)
+	h.addCard(c3)
 
 	if h.pickCard() != c1 {
 		t.Fatal("test fale")
 	}
 
-	if h.hand_[0] != c2 {
+	if h.hand_[0] != c2 || h.hand_[1] != c3 {
 		t.Fatal("test fale")
 	}
 }
