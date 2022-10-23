d58a76a0bebc5d74ec59972ee1e560885c8bcffc
tomcha <tomcha@tomcha.net>
2022-10-24 08:18:46 +0900

    shuffle 実装

--- hand.go
+++ hand.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Hand struct {
 	hand_ []Card
 }
@@ -13,4 +18,21 @@ func (h *Hand) pickCard() (c Card) {
 	h.hand_ = h.hand_[1:]
 	return pickedCard
 
+}
+
+func (h *Hand) shuffle() {
+	size := len(h.hand_)
+	for i := 0; i < size*2; i++ {
+		rand.Seed(time.Now().UnixNano())
+		pos := rand.Intn(size)
+		if pos == 0 {
+			c := h.hand_[0]
+			h.hand_ = h.hand_[1:]
+			h.hand_ = append(h.hand_, c)
+		} else if pos != size {
+			c := h.hand_[pos]
+			newHand := append(h.hand_[0:pos], h.hand_[pos+1:size]...)
+			h.hand_ = append(newHand, c)
+		}
+	}
 }
