4270b5c3f487b1d2e73c2667cffbc056e346cbbf
tomcha <tomcha@tomcha.net>
2022-10-24 08:13:06 +0900

    shuffle テスト追加

--- hand_test.go
+++ hand_test.go
@@ -35,3 +35,20 @@ func TestPickCard(t *testing.T) {
 		t.Fatal("test fale")
 	}
 }
+
+func TestShuffle(t *testing.T) {
+	c1 := Card{SUIT_DIAMOND, 10}
+	c2 := Card{SUIT_CLUB, 3}
+	c3 := Card{SUIT_HEART, 12}
+
+	h := Hand{}
+	h.addCard(c1)
+	h.addCard(c2)
+	h.addCard(c3)
+
+	h.shuffle()
+	if len(h.hand_) != 3 {
+		t.Fatal("test fale")
+	}
+
+}
