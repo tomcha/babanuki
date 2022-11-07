43748d0833d89a8f246d8d04ff2cf68680254780
tomcha <tomcha@tomcha.net>
2022-11-08 08:25:38 +0900

    tableストラクト テスト生成

--- table_test.go
+++ table_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestDisposeCard(t *testing.T) {
+	table := Table{}
+	c := Card{SUIT_SPADE, 10}
+	table.disposeCard(c)
+	if table.cards[len(cards)] != c {
+		t.Fatal("test fale")
+	}
+}
