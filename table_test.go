bfff2a3d5151744c0443655dd006760184bd7e77
tomcha <tomcha@tomcha.net>
2022-11-09 08:16:57 +0900

    disposeCardメソッドのテスト実装

--- table_test.go
+++ table_test.go
@@ -4,9 +4,24 @@ import "testing"
 
 func TestDisposeCard(t *testing.T) {
 	table := Table{}
+	table.cards_ = []Card{}
 	c := Card{SUIT_SPADE, 10}
 	table.disposeCard(c)
-	if table.cards[len(cards)] != c {
+	if table.cards_[len(table.cards_)-1] != c {
 		t.Fatal("test fale")
 	}
+}
+
+func TestDisposeCard2(t *testing.T) {
+	table := Table{}
+	table.cards_ = []Card{
+		Card{SUIT_DIAMOND, 1},
+		Card{SUIT_CLUB, 3},
+	}
+	c := Card{SUIT_SPADE, 7}
+	table.disposeCard(c)
+	if table.cards_[len(table.cards_)-1] != c {
+		t.Fatal("test fale")
+	}
+
 }
