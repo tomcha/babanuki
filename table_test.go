664871edf4d9cd23ccb22e78b0b3061d7408b20e
tomcha <tomcha@tomcha.net>
2022-11-11 07:57:25 +0900

    ストラクト宣言をfix

--- table_test.go
+++ table_test.go
@@ -15,13 +15,12 @@ func TestDisposeCard(t *testing.T) {
 func TestDisposeCard2(t *testing.T) {
 	table := Table{}
 	table.cards_ = []Card{
-		Card{SUIT_DIAMOND, 1},
-		Card{SUIT_CLUB, 3},
+		{SUIT_DIAMOND, 1},
+		{SUIT_CLUB, 3},
 	}
 	c := Card{SUIT_SPADE, 7}
 	table.disposeCard(c)
 	if table.cards_[len(table.cards_)-1] != c {
 		t.Fatal("test fale")
 	}
-
 }
