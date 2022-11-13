ba11a2def22789803c58fe9f391e792c3a650538
tomcha <tomcha@tomcha.net>
2022-11-13 21:05:17 +0900

    メソッド名fix

--- table.go
+++ table.go
@@ -4,6 +4,6 @@ type Table struct {
 	cards_ []Card
 }
 
-func (t *Table) disposeCard(c Card) {
+func (t *Table) DisposeCard(c Card) {
 	t.cards_ = append(t.cards_, c)
 }
--- table_test.go
+++ table_test.go
@@ -6,7 +6,7 @@ func TestDisposeCard(t *testing.T) {
 	table := Table{}
 	table.cards_ = []Card{}
 	c := Card{SUIT_SPADE, 10}
-	table.disposeCard(c)
+	table.DisposeCard(c)
 	if table.cards_[len(table.cards_)-1] != c {
 		t.Fatal("test fale")
 	}
@@ -19,7 +19,7 @@ func TestDisposeCard2(t *testing.T) {
 		{SUIT_CLUB, 3},
 	}
 	c := Card{SUIT_SPADE, 7}
-	table.disposeCard(c)
+	table.DisposeCard(c)
 	if table.cards_[len(table.cards_)-1] != c {
 		t.Fatal("test fale")
 	}
