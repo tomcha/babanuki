5fcd9150281d37d2cc82eb54a8a766764a563bcd
tomcha <tomcha@tomcha.net>
2022-11-21 17:53:20 +0900

    ShowHand テスト実装

--- player_test.go
+++ player_test.go
@@ -8,8 +8,41 @@ func TestPlay(t *testing.T) {
 
 }
 
-func TesstShowHand(t *testing.T) {
-
+func TestShowHand(t *testing.T) {
+	h := new(Hand)
+	cards := []Card{
+		{SUIT_CLUB, 1},
+		{SUIT_CLUB, 2},
+		{SUIT_CLUB, 3},
+		{SUIT_CLUB, 4},
+		{SUIT_CLUB, 5},
+		{SUIT_CLUB, 6},
+		{SUIT_CLUB, 7},
+		{SUIT_CLUB, 8},
+		{SUIT_CLUB, 9},
+		{SUIT_CLUB, 10},
+		{SUIT_CLUB, 11},
+		{SUIT_CLUB, 12},
+		{SUIT_CLUB, 13},
+	}
+	cards2 := make([]Card, len(cards))
+	copy(cards2, cards)
+	h.hand_ = cards2
+	p := Player{
+		name_:  "taro",
+		myHand: h,
+		table_: new(Table),
+	}
+	p.ShowHand()
+	if len(p.myHand.hand_) != len(cards) {
+		t.Fatal("test fale")
+	}
+	for i := 0; i < len(p.myHand.hand_); i++ {
+		if p.myHand.hand_[i] != cards[i] {
+			return
+		}
+	}
+	t.Fatal("test fale")
 }
 
 func TestRecieveCard(t *testing.T) {
