fbc1e1fbc4052390cc10d9b362c1b1ede92c665b
tomcha <tomcha@tomcha.net>
2022-11-22 19:38:20 +0900

    master declareWin実装

--- master.go
+++ master.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Master struct {
 	players_ []Player
+}
+
+func (m *Master) declareWin(p Player) {
+	fmt.Printf("%s is finnished\n", p.name_)
+	var ti int
+	for i := 0; i < len(m.players_); i++ {
+		if m.players_[i] == p {
+			ti = i
+			break
+		}
+	}
+	if ti == len(m.players_)-1 {
+		m.players_ = m.players_[:ti]
+	} else {
+		m.players_ = append(m.players_[:ti], m.players_[ti+1:]...)
+	}
 }
