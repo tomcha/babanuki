f534e0093467bced5eaaa0db31f280956bf442ca
tomcha <tomcha@tomcha.net>
2022-11-22 19:37:38 +0900

    master declareWin テスト実装

--- master_test.go
+++ master_test.go
@@ -4,5 +4,21 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestDeclareWin(t *testing.T) {
+	m := new(Master)
+	p := Player{
+		name_:  "taro",
+		myHand: new(Hand),
+		table_: &Table{
+			cards_: []Card{},
+		},
+	}
+	m.players_ = append(m.players_, p)
+	m.players_ = append(m.players_, p)
+	m.players_ = append(m.players_, p)
+	pn := len(m.players_)
+	m.declareWin(p)
+	if pn != len(m.players_)+1 {
+		t.Fatal("test fale")
+	}
 }
