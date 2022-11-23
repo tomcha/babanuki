97c9c58dfdb80be000029f4bac18d8ecd88587de
tomcha <tomcha@tomcha.net>
2022-11-24 08:17:38 +0900

    master ResisterPlayer テスト実装

--- master_test.go
+++ master_test.go
@@ -17,8 +17,27 @@ func TestDeclareWin(t *testing.T) {
 	m.players_ = append(m.players_, p)
 	m.players_ = append(m.players_, p)
 	pn := len(m.players_)
-	m.declareWin(p)
+	m.DeclareWin(p)
 	if pn != len(m.players_)+1 {
 		t.Fatal("test fale")
+	}
+}
+
+func TestRegisterPlayer(t *testing.T) {
+	m := new(Master)
+	p1 := Player{
+		name_:  "taro",
+		myHand: &Hand{},
+		table_: &Table{},
+	}
+	p2 := Player{
+		name_:  "hanako",
+		myHand: &Hand{},
+		table_: &Table{},
+	}
+	m.RegisterPlayer(p1)
+	m.RegisterPlayer(p2)
+	if len(m.players_) != 2 {
+		t.Fatal("test fatal")
 	}
 }
