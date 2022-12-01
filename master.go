a4ccdeb1683b721f39b3cfc58b95dfba1740d8cb
tomcha <tomcha@tomcha.net>
2022-12-02 08:06:29 +0900

    StartGame 実装

--- master.go
+++ master.go
@@ -37,3 +37,26 @@ func (m *Master) DeclareWin(p Player) {
 func (m *Master) RegisterPlayer(p Player) {
 	m.players_ = append(m.players_, p)
 }
+
+func (m *Master) StartGame() {
+	fmt.Println("ババ抜きを開始します")
+	playersSize := len(m.players_)
+	for i := 0; playersSize > 1; i++ {
+		playersSize = len(m.players_)
+		playerIndex := i % playersSize
+		nextPlayerIndex := (i + 1) % playersSize
+		fmt.Printf("%s さんの番です\n", m.players_[playerIndex])
+		m.players_[playerIndex].Play(&m.players_[nextPlayerIndex])
+		m.LookAllPlayersHand()
+		//		time.Sleep(time.Second * 2)
+	}
+	fmt.Println("ババ抜きを終了します")
+}
+
+func (m *Master) LookAllPlayersHand() {
+	ps := len(m.players_)
+	for i := 0; i < ps; i++ {
+		fmt.Printf("%s :", m.players_[i])
+		fmt.Printf(" %s\n", m.players_[i].myHand.hand_)
+	}
+}
